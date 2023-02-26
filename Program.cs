35d2b4261cdc1327e5b14ca31698c1ff502c0a46
kjkasi <5842602+kjkasi@users.noreply.github.com>
2023-02-26 21:40:04 +0300

    Add project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AsyncBreakfast
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
